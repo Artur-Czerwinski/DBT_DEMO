ffa3b92e3e623eb867f7e27a9c2ea64c329c5e42
Artur Czerwiński <artczerwo33@gmail.com>
2025-08-12 14:51:24 +0000

    Changing name from state_dimension to station dimension

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -5,8 +5,7 @@ with bike as (
     START_STATION_ID,
     START_STATION_NAME,
     start_lat,
-    start_lng,
-    end_station_name
+    start_lng
     from {{ source('demo', 'bike') }}
     where ride_id != 'ride_id'
     limit 10

51ad4b68a6dfb426f058fefc9d284fedacaa088e
Artur Czerwiński <artczerwo33@gmail.com>
2025-08-12 14:48:49 +0000

    state dimension created

--- models/state_dimension.sql
+++ models/state_dimension.sql
@@ -5,7 +5,8 @@ with bike as (
     START_STATION_ID,
     START_STATION_NAME,
     start_lat,
-    start_lng
+    start_lng,
+    end_station_name
     from {{ source('demo', 'bike') }}
     where ride_id != 'ride_id'
     limit 10
